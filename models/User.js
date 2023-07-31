b9d473eaabdf3f794da926e5ba86df3ce136fc27
Suazo3000 <suazosteve1@gmail.com>
2023-07-30 20:53:13 -0500

    renamed files updated user and controllers files

--- models/User.js
+++ models/User.js
@@ -3,19 +3,32 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
-    videos: [
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'video',
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
       },
     ],
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
@@ -23,21 +36,13 @@ const userSchema = new Schema(
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+// Increases friend count in User model object when friends are added by a user
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-// Initialize our User model
-const User = model('user', userSchema);
+// Creates User model with userSchema
+const User = model("user", userSchema);
 
+// Exports
 module.exports = User;
