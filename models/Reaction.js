dcb5fdcf161a8313c77201906c06a2b584c68c12
Suazo3000 <suazosteve1@gmail.com>
2023-07-30 20:56:50 -0500

    revised file name to reaction and updated the code

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+const Response = require('./Response');
+
+// Schema to create Post model
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Exports
+module.exports = reactionSchema;
